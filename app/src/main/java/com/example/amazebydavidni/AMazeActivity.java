40b1c581047f7ae69b32f8319d0054368ac101a6
dni <dni@email.wm.edu>
2021-11-14 22:26:39 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/amazebydavidni/AMazeActivity.java
@@ -0,0 +1,14 @@
+package com.example.amazebydavidni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AMazeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_maze);
+    }
+}
