ce147b4e2efc5570213744898692d44d7c6f72ca
dni <dni@email.wm.edu>
2021-11-20 05:49:09 -0500

    Implemented PlayAnimationActivity UI logic

--- app/src/main/java/com/example/amazebydavidni/PlayAnimationActivity.java
+++ app/src/main/java/com/example/amazebydavidni/PlayAnimationActivity.java
@@ -3,12 +3,17 @@ package com.example.amazebydavidni;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -22,13 +27,31 @@ public class PlayAnimationActivity extends AppCompatActivity {
     // Robot sensor configuration
     private String robotConfig;
 
-    // Starting energy of the robot
-    private int totalEnergy;
+    // Speed of the animation, defaulted to 0
+    private int speed = 0;
+    // Starting energy of the robot, defaulted to 3500
+    private int initialEnergy = 3500;
     // Current energy consumed by the robot
     private int energyConsumed;
 
+    // ProgressBar displaying the robot's energy
+    private ProgressBar energyProgress;
     // Toggle button for showing map (whole maze with solution and visible walls) during animation/automatic play
     private ToggleButton mapButton;
+    // Images representing robot's sensors (green = operational and red = non-operational)
+    // Flags representing robot's operational status (true if operational, false otherwise)
+    // Forward sensor
+    ImageView fSensor;
+    boolean fSensorStatus = true;
+    // Back sensor
+    ImageView bSensor;
+    boolean bSensorStatus = true;
+    // Left sensor
+    ImageView lSensor;
+    boolean lSensorStatus = true;
+    // Right sensor
+    ImageView rSensor;
+    boolean rSensorStatus = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -116,12 +139,58 @@ public class PlayAnimationActivity extends AppCompatActivity {
             }
         });
 
-        // TODO Figure out how to handle speed
-        // TODO Get correct speed value from SeekBar (R.id.speedBar)
+        // SeekBar for animation speed
+        // TODO P7: figure out appropriate range for speed (currently 0-2) and to change animation speed
+        SeekBar speedSeekBar = findViewById(R.id.speedBar);
+        TextView speedText = findViewById(R.id.speedText);
+        // SeekBar Listener
+        speedSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            /**
+             * This method listens to changes made to the SeekBar for animation speed.
+             * Gets the desired speed of the animation from the SeekBar's progress value.
+             * Updates text to show currently selected animation speed.
+             * Prints Logcat verbose message for debugging purposes.
+             * @param seekBar SeekBar that determines the size of the maze
+             * @param progress Current progress level of the SeekBar, value is the size of the maze
+             * @param fromUser True if progress change was initiated by the user
+             */
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                String output = "Set Speed: " + progress;
+                speedText.setText(output);
+                // Toast message about the animation speed selected on the SeekBar, for debugging
+                Toast.makeText(getApplicationContext(), "Speed now: " + progress, Toast.LENGTH_SHORT).show();
+                // Log message about the animation speed selected on the SeekBar, for debugging
+                Log.v("PlayAnimationActivity","Speed set to : " + progress);
+                speed = progress;
+            }
 
-        // TODO Update progress bar to reflect robot's energy
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        // Progress bar representing robot's energy
+        energyProgress = findViewById(R.id.robotEnergyBar);
+        // TODO P7: Have energy bar update along with game instead of one time call (currently used for debugging P6)
+        updateEnergyBar();
 
         // TODO Write code such that sensor images change color to reflect status
+        // ImageViews representing operational status of robot's sensors
+        this.fSensor = findViewById(R.id.fSensor);
+        this.bSensor = findViewById(R.id.bSensor);
+        this.lSensor = findViewById(R.id.leftSensor);
+        this.rSensor = findViewById(R.id.rightSensor);
+
+        // TODO P7 have sensors call updateSensorImages during game instead of using one time call (currently used for debugging P6)
+        // Check if updateSensorImages correctly sets sensors to green if they are operational
+        updateSensorImages();
+        // Toast message about the animation speed selected on the SeekBar, for debugging
+        Toast.makeText(getApplicationContext(), "Testing Sensor UI, with all Sensors Operational", Toast.LENGTH_SHORT).show();
 
         // ImageButton for going to the win activity. Placeholder for maze game.
         Button goToWin = findViewById(R.id.Go2Winning);
@@ -158,6 +227,60 @@ public class PlayAnimationActivity extends AppCompatActivity {
                 goToLosing(400,500,600);
             }
         });
+    }
+
+    /**
+     * Helper method which updates UI to show sensor's status.
+     * If a sensor is non-operation, its UI representation will be given a red tint.
+     * If a sensor is operational, its UI representation will be given a green tint.
+     */
+    private void updateSensorImages() {
+        Log.v("PlayAnimationActivity","Updating sensor UI color to represent operational status");
+        // Set forward sensor color
+        if (this.fSensorStatus) {
+            this.fSensor.setColorFilter(Color.argb(255, 80, 220, 100)); // Green Tint
+        }
+        else {
+            this.fSensor.setColorFilter(Color.argb(255, 255, 36, 0)); // Red Tint
+        }
+        // Set backwards sensor color
+        if (this.bSensorStatus) {
+            this.bSensor.setColorFilter(Color.argb(255, 80, 220, 100)); // Green Tint
+        }
+        else {
+            this.bSensor.setColorFilter(Color.argb(255, 255, 36, 0)); // Red Tint
+        }
+        // Set left sensor color
+        if (this.lSensorStatus) {
+            this.lSensor.setColorFilter(Color.argb(255, 80, 220, 100)); // Green Tint
+        }
+        else {
+            this.lSensor.setColorFilter(Color.argb(255, 255, 36, 0)); // Red Tint
+        }
+        // Set right sensor color
+        if (this.rSensorStatus) {
+            this.rSensor.setColorFilter(Color.argb(255, 80, 220, 100)); // Green Tint
+        }
+        else {
+            this.rSensor.setColorFilter(Color.argb(255, 255, 36, 0)); // Red Tint
+        }
+    }
+
+    /**
+     * Helper method used to update the robot's energy bar.
+     * Sets the robot's energy bar to represent the percentage of energy still left (current energy/initial energy).
+     */
+    private void updateEnergyBar() {
+        // Current energy consumed by the robot, used hardcoded value of 1000 until P7
+        // TODO P7: get energyConsumed from RobotDriver.getEnergyConsumption()
+        this.energyConsumed = 1000;
+        float percentage =  (((float)this.initialEnergy - this.energyConsumed)/(float)this.initialEnergy)* 100;
+        this.energyProgress.setProgress((int)percentage);
+
+        // Toast message of robot's energy bar changing, used for debugging
+        Toast.makeText(getApplicationContext(), "Test Setting Robot's energy bar set to : " + (int)percentage + "%", Toast.LENGTH_SHORT).show();
+        // Log message of robot's energy bar percentage changing, used for debugging
+        Log.v("PlayAnimationActivity","Robot's energy bar set to : " + (int)percentage + "%");
     }
 
     /**
