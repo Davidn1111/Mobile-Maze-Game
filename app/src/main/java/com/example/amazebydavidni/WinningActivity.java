281e97a37fa4c28565c54ef97d743a2231c58b2d
dni <dni@email.wm.edu>
2021-11-20 06:30:47 -0500

    Implemented UI logic for WinningActivity

--- app/src/main/java/com/example/amazebydavidni/WinningActivity.java
+++ app/src/main/java/com/example/amazebydavidni/WinningActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class WinningActivity extends AppCompatActivity {
@@ -32,5 +33,27 @@ public class WinningActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), "You won the game with \nPath Length: " + this.pathLength+ ", ShortestPath: " + this.shortestPath + ",energyConsumption: " + this.energyConsumption, Toast.LENGTH_SHORT).show();
         // Log message that displays journey information received from playing activity, for debugging purposes
         Log.v("WinningActivity","Received the following information:\nPath Length: " + this.pathLength+ ", ShortestPath: " + this.shortestPath + ",energyConsumption: " + this.energyConsumption);
+
+        // Set the winning screen's text to match given journey information
+        // TextView for path length
+        TextView pathLengthText = findViewById(R.id.pathLengthText);
+        pathLengthText.setText("Length of Path: " + this.pathLength);
+
+        // TextView for shortest path
+        TextView shortestPathText = findViewById(R.id.shortestPathText);
+        shortestPathText.setText("Length of Shortest Path: "+ this.shortestPath);
+
+        // TextView for energy consumption
+        TextView energyConsumptionText = findViewById(R.id.energyConsumptionText);
+        // Display energy consumption if robot was used/energy consumption is >= 0
+        if (this.energyConsumption >= 0){
+            energyConsumptionText.setText("Energy Consumed: " + this.energyConsumption);
+        }
+        // If manual mode was used, hide the energy consumption text
+        else {
+            energyConsumptionText.setText("");
+        }
+
+
     }
 }
